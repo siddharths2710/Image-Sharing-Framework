3fea3d7c056612b25f69f2a425cab71f4fc15921
Shreyash S Sarnayak <shreyash14s+gitlab@gmail.com>
2016-12-28 23:56:16 +0530

    Modified some files

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var fs = require('fs');
 var url = require('url');
 var express = require('express');
 
-
 // Create a server
-http.createServer( function (request, response) {  
+http.createServer( function (request, response) {
    var pathname = url.parse(request.url).pathname;
-   
+
    console.log("Request for " + pathname + " received.");
-   
+
    // Read the requested file content from file system
    fs.readFile(pathname.substr(1), function (err, data) {
       if (err) {
@@ -17,27 +16,31 @@ http.createServer( function (request, response) {
          // HTTP Status: 404 : NOT FOUND
          // Content Type: text/plain
          response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{	
-        //Page found	  
+         response.end();
+      } else {
+        //Page found
         // HTTP Status: 200 : OK
         // Content Type: text/plain
-        response.writeHead(200, {'Content-Type': 'text/html'});	
+        if (pathname.includes(".html")) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+        } else if (pathname.includes(".css")) {
+            response.writeHead(200, {'Content-Type': 'text/css'});
+        }
 
         //var r = /\/image\/[0-9]*\.jpg/i;
         var r = /\/images\/[0-9]*\.jpg/i;
-        console.log(r, pathname, r.test(pathname))
+        console.log(r, pathname, r.test(pathname));
         if (r.test(pathname)) {
           var p = pathname.substring(1);
-          var img = fs.readFileSync("."+pathname);
+          var img = fs.readFileSync("." + pathname);
           console.log(pathname);
           response.writeHead(200, {'Content-Type': 'image/jpg' });
           response.end(img, 'binary');
-        } else
-        response.write(data.toString());		
-        // Send the response body 
-        response.end();
+        } else {
+          response.end(data.toString());
+        }
       }
-   });   
+   });
 }).listen(8081);
 
 // Console will print the message
